0721208e5cf1c4128c42281fc02f32c6fd998939
BreElle Wells <breellejordyn@gmail.com>
2023-08-11 17:14:54 -0600

    fetch movie data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import AddMovie from "./AddMovie";
 import MainContent from "./MainContent";
 
+const baseUrl = "http://localhost:3000"
+const moviesUrl = baseUrl + '/movies'
+
 function App() {
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    fetch(moviesUrl)
+      .then(r => r.json())
+      .then(data => setMovieList(data))
+  })
+
   return (
     <>
       <Navbar />
