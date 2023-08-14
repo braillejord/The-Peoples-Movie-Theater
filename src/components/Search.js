8f874f5b7b18913fee8d95816a5a6f04231dd8e6
taby0402 <132953981+taby0402@users.noreply.github.com>
2023-08-13 23:14:03 -0400

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
 
-function Search() {
-    return (
-        <>
-
-        </>
-    );
-}
+const Search = (props) => {
+	return (
+		<div className='col col-sm-4'>
+			<input
+				className='form-control'
+				value={props.value}
+				onChange={(event) => props.setSearchValue(event.target.value)}
+				placeholder='Type to search...'
+			></input>
+		</div>
+	);
+};
 
 export default Search;
+
+
+
