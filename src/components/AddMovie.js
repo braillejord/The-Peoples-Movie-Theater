18df253fbae12338639d07cc757cfa875b45a73f
BreElle Wells <breellejordyn@gmail.com>
2023-08-15 09:31:33 -0600

    style add movie form

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -2,38 +2,52 @@ import React from "react";
 
 function AddMovie() {
     return (
-        <div className="ui form">
-            <div className="six fields">
-                <div className="field">
-                    <label>Movie Title</label>
-                    <input type="text" placeholder="Movie Title" />
+        <>
+            <h1 className="ui center aligned block header">Add a Movie 🍿</h1>
+            <form className="ui container form">
+                <div className="two fields">
+                    <div className="required field">
+                        <label>Movie Title</label>
+                        <input type="text" placeholder="Movie Title" />
+                    </div>
+                    <div className="required field">
+                        <label>Movie Image</label>
+                        <input type="text" placeholder="Movie Image" />
+                    </div>
                 </div>
-                <div className="field">
-                    <label>Movie Image</label>
-                    <input type="text" placeholder="Movie Image" />
+                <div className="three fields">
+                    <div className="required field">
+                        <label>Movie Rating</label>
+                        <select className="ui fluid search dropdown">
+                            <option value='G'>G</option>
+                            <option value='PG'>PG</option>
+                            <option value='PG-13'>PG-13</option>
+                            <option value='R'>R</option>
+                            <option value='NR'>NR</option>
+                        </select>
+                    </div>
+                    <div className="required field">
+                        <label>Movie Genre</label>
+                        <select className="ui fluid search dropdown">
+                            <option value='comedy'>Comedy</option>
+                            <option value='thriller/horror'>Thriller/Horror</option>
+                            <option value='action/adventure'>Action/Adventure</option>
+                            <option value='family'>Family</option>
+                            <option value='romance'>Romance</option>
+                        </select>
+                    </div>
+                    <div className="required field">
+                        <label>Movie Ticket Price</label>
+                        <input type="number" step="0.01" placeholder="Movie Ticket Price" />
+                    </div>
                 </div>
-                <div className="field">
-                    <label>Movie Rating</label>
-                    <input type="text" placeholder="Movie Rating" />
-                </div>
-                <div className="field">
+                <div className="required field">
                     <label>Movie Description</label>
-                    <input type="text" placeholder="Movie Description" />
+                    <textarea></textarea>
                 </div>
-                <div className="field">
-                    <label>Movie Genre</label>
-                    <input type="text" placeholder="Movie Genre" />
-                </div>
-                <div className="field">
-                    <label>Movie Ticket Price</label>
-                    <input type="text" placeholder="Movie Ticket Price" />
-                </div>
-                <div>
-                    <label>Submit Movie</label>
-                    <button className="ui button" type="submit">🍿</button>
-                </div>
-            </div>
-        </div>
+                <button className="ui button" type="submit">Add Movie 🎥</button>
+            </form >
+        </>
     );
 }
 
