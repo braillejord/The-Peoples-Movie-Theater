c3c4102871439ebbbef156cacb94258046bb9340
BreElle Wells <breellejordyn@gmail.com>
2023-08-17 08:30:55 -0600

    add dropdown

--- src/components/GenreDropdown.js
+++ src/components/GenreDropdown.js
@@ -1,42 +1,56 @@
 import React, { useState } from "react";
-import GenreDropdown from "./GenreDropdown";
 
-const SelectedMovie = ({ name, genre }) => (
-    <div className="item-container">
-        <div>
-            <span className="item-label">Name:</span>
-            {name}
-        </div>
-        <div>
-            <span className="item-label">Genre:</span>
-            {genre}
-        </div>
-    </div>
-);
-
-const App = () => {
-    const [value, setValue] = useState("Horror");
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    };
-
+function GenreDropdown() {
     return (
-        <div>
-            <label>
-                What Movie Genre do you Want to See?
-                <select value={value} onChange={handleChange} className="ui dropdown">
-                    <option value="Horror">Horror</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Family">Family</option>
-                    <option value="Thriller">Thriller</option>
-                    <option value="Romance">Romance</option>
-                </select>
-            </label>
-            <p>We choose {value}!</p>
-            <SelectedMovie name="Example Movie" genre={value} />
-        </div>
-    );
-};
+        <>
+            <select className="ui dropdown">
+                <option value="Comedy">Comedy</option>
+                <option value="Thriller/Horror">Thriller/Horror</option>
+                <option value="Action/Adventure">Action/Adventure</option>
+                <option value="Family">Family</option>
+                <option value="Romance">Romance</option>
+            </select>
+        </>
+    )
+}
+
+
+// const SelectedMovie = ({ name, genre }) => (
+//     <div className="item-container">
+//         <div>
+//             <span className="item-label">Name:</span>
+//             {name}
+//         </div>
+//         <div>
+//             <span className="item-label">Genre:</span>
+//             {genre}
+//         </div>
+//     </div>
+// );
+
+// const App = () => {
+//     const [value, setValue] = useState("Horror");
+
+//     const handleChange = (event) => {
+//         setValue(event.target.value);
+//     };
+
+//     return (
+//         <div>
+//             <label>
+//                 What Movie Genre do you Want to See?
+//                 <select value={value} onChange={handleChange} className="ui dropdown">
+//                     <option value="Horror">Horror</option>
+//                     <option value="Comedy">Comedy</option>
+//                     <option value="Family">Family</option>
+//                     <option value="Thriller">Thriller</option>
+//                     <option value="Romance">Romance</option>
+//                 </select>
+//             </label>
+//             <p>We choose {value}!</p>
+//             <SelectedMovie name="Example Movie" genre={value} />
+//         </div>
+//     );
+// };
 
 export default GenreDropdown;
